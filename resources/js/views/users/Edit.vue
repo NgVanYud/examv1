e783e2345b47a3affb498067911086768dddf7b9
ngduy <duy.nv.96@gmail.com>
2019-05-20 00:22:00 +0700

    Validate the form edit user

--- resources/js/views/users/Edit.vue
+++ resources/js/views/users/Edit.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="user" label-width="120px">
-      <el-form-item label="Tài Khoản">
-        <el-input v-model="user.username"/>
+    <el-form ref="editForm" :model="user" :rules="rules" label-width="120px">
+      <el-form-item label="Tài Khoản" prop="username">
+        <el-input v-model="user.username" :disabled="true"/>
       </el-form-item>
-      <el-form-item label="Mã Số">
+      <el-form-item label="Mã Số" prop="code">
         <el-input v-model="user.code"/>
       </el-form-item>
-      <el-form-item label="Họ">
+      <el-form-item label="Họ" prop="last_name">
         <el-input v-model="user.last_name"/>
       </el-form-item>
-      <el-form-item label="Tên">
+      <el-form-item label="Tên" prop="first_name">
         <el-input v-model="user.first_name"/>
       </el-form-item>
-      <el-form-item label="Email">
+      <el-form-item label="Email" prop="email">
         <el-input v-model="user.email"/>
       </el-form-item>
-      <el-form-item label="Kích Hoạt">
+      <el-form-item label="Kích Hoạt" prop="active">
         <el-switch v-model="user.active"></el-switch>
       </el-form-item>
-      <el-form-item label="Quyền">
+      <el-form-item label="Quyền" prop="roles">
         <el-select v-model="userRoleIds" placeholder="Chọn Quyền" multiple>
           <el-option v-for="item in roles" :label="item.name" :value="item.id" :key="item.id"/>
         </el-select>
@@ -47,15 +47,26 @@ export default {
       roles: [],
       userRoleIds: [],
       permissionIds: [],
-      form: {
-        // username: this.user.username,
-        code: '',
-        email: '',
-        // last_name: this.user.last_name,
-        // first_name: this.user.first_name,
-        // roles: '',
-        // permission: [],
-        // active: this.user.active,
+      rules: {
+        code: [
+          { required: true, message: 'Vui lòng nhập mã số người dùng.', trigger: ['change', 'blur'] },
+          { min: 3, max: 15, message: 'Độ dài mã số người dùng từ 3 đến 15 ký tự.', trigger: ['change', 'blur'] },
+        ],
+        first_name: [
+          { required: true, message: 'Vui lòng nhập tên người dùng.', trigger: ['change', 'blur'] },
+          { min: 1, max: 20, message: 'Độ dài tên người dùng từ 1 đến 20 ký tự.', trigger: ['change', 'blur'] },
+        ],
+        last_name: [
+          { required: true, message: 'Vui lòng nhập họ người dùng.', trigger: ['change', 'blur'] },
+          { min: 2, max: 60, message: 'Độ dài họ người dùng từ 2 đến 60 ký tự.', trigger: ['change', 'blur'] },
+        ],
+        email: [
+          { type: 'email', required: true, message: 'Vui lòng nhập email người dùng.', trigger: ['change', 'blur'] },
+          { min: 3, max: 100, message: 'Độ dài email từ 3 đến 100 ký tự.', trigger: ['change', 'blur'] },
+        ],
+        roles: [
+          { type: 'array', required: true, message: 'Vui lòng nhập email người dùng.', trigger: ['change', 'blur'] },
+        ],
       },
     };
   },
